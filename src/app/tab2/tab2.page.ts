b04ec8595feac022a48b8dfed084cc92d6b94c95
Van Say Nguyen <vansay@hotmail.fr>
2020-09-21 22:49:14 +0200

    Mise en place API service

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ApiService } from "../api.service";
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  selector: "app-tab2",
+  templateUrl: "tab2.page.html",
+  styleUrls: ["tab2.page.scss"],
 })
 export class Tab2Page {
+  pokemons;
+  constructor(private apiService: ApiService) {}
 
-  constructor() {}
-
+  ionViewDidEnter() {
+    this.apiService.getPokemons().subscribe((data) => {
+      console.log(data);
+      this.pokemons = data.results;
+    });
+  }
 }
